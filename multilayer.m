1263407d3cc8421689eb2cfd0bab34ae4af8c67a
foviedoITBA <foviedo@itba.edu.ar>
2018-03-19 14:32:30 -0300

    run_pattern ahora calcula y devuelve la corrección a hacer sobre los pesos

--- multilayer.m
+++ multilayer.m
@@ -7,19 +7,58 @@
 %
 % E is a column vector. E(i) holds the input for unit i
 %
-% g is a cell array of function handles. g{m} is the the activation function for
-% layer m
+% g is a two dimentional cell array of function handles. g{m}{1} is the the 
+% activation function for layer m; g{m}{2} is the derivative of g{m}{1} in 
+% terms of g
+%
+% S is the expected output as a column vector
+%
+% eta is the learning rate
 
 % Return value:
 %
-% V is a cell array of column vectors. V{m}(i) hols the output of unit i at
-% layer m-1
+% dw is a cell array of matrices. dw{m} contains the updates to be added to the 
+% weight matrix at layer m
 
-function V = run_pattern(W, E, g)
-  V = cell(numel(W)+1, 1);
+function dw = run_pattern(W, E, g, S, eta)
+  % M is the number of layers
+  M = numel(W);
+  % V is a cell array of column vectors. V{m}(i) hols the output of unit i at
+  % layer m-1 
+  V = cell(M+1, 1);
   V{1} = E;
-  for k = [1:numel(W)]
+  for k = [1:M]
     V{k} = [-1; V{k}];
-    V{k+1} = arrayfun(g{k}, W{k}*V{k});
+    V{k+1} = arrayfun(g{k}{1}, W{k}*V{k});
+  endfor
+  
+  % ders is a cell array. ders{m}(i) contains the derivative of the mth layer 
+  % activation function evaluated at the ith unit's h
+  ders = V;
+  for k = [2:M]
+    ders{k} = arrayfun(g{k-1}{2}, V{k}(2:end));
+  endfor
+  ders{M+1} = arrayfun(g{k-1}{2}, V{k});
+  
+  % delta is a cell array.
+  delta = cell(M, 1);
+  delta{M} = direct_product(S - V{M+1}, ders{M+1});
+  for k = [M-1:-1:1]
+    delta{k} = direct_product((delta{k+1}*W{k+1})(2:end), ders{k+2});
+  endfor
+  
+  % dw is a cell array of matrices; dw{m} contains the weight update matrix for 
+  % layer m
+  dw = cell(M, 1);
+  for k = [1:M]
+    dw{k} = eta*(V{k}*delta{k})';
+  endfor
+  
+endfunction
+
+function out = direct_product(v, w)
+  out = [];
+  for k = [1:numel(v)]
+    out = [out v(k)*w(k)];
   endfor
 endfunction
