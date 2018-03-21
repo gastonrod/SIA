1beb4a925fa4f7c057fb4295f9e1dabdf01073e2
Gaston Rodriguez <grodriguez@itba.edu.ar>
2018-03-21 15:25:51 -0300

    Mejore el test y el load_file

--- load_file.m
+++ load_file.m
@@ -1,7 +1,13 @@
-function [x y z] = load_file(fn)
-  data = load('-ascii', fn)
-  length = length(data / 3)
-  x = data(1:length)
-  y = data(length+1:length*2)
-  z = data(length*2+1:length*3)
+function info = load_file(fn)
+  data = load('-ascii', fn);
+  length = length(data / 3);
+  x = data(1:length);
+  y = data(length+1:length*2);
+  z = data(length*2+1:length*3);
+  info = {};
+  for k = [1:length]
+    info{k}{1}(1) = x(k)
+    info{k}{1}(2) = y(k)
+    info{k}{2} = z(k)
+  end
 end
--- test.m
+++ test.m
@@ -1,13 +1,14 @@
-function test(x,y,z)
+% patterns{k} : Input - Expected output pair
+function test(patterns)
   eps = 1e-05;
   err_count = 0;
-  for k = [1:numel(x)]
+  for k = [1:numel(patterns)]
     % run : Give the input to the neural network and get an answer.
-    ans = run(x(k),y(k));
-    if abs(ans- z(k)) > eps
+    ans = run(patterns{k}{1});
+    if abs(ans- patterns{k}{2}) > eps
       err_count++;
     endif
   end
   %print error percentage
-  printf("%f%% of wrong answers\n", err_count / numel(x));
+  printf("%f%% of wrong answers\n", err_count / numel(patterns) * 100);
 end
