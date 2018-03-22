d87a302945f7c11512b7f62543612a8374924367
Gaston <grodriguez@itba.edu.ar>
2018-03-22 12:00:55 -0300

    Added a xor-setup file. I don't think it works though

--- load_file.m
+++ load_file.m
@@ -6,8 +6,8 @@ function info = load_file(fn)
   z = data(length*2+1:length*3);
   info = {};
   for k = [1:length]
-    info{k}{1}(1) = x(k)
-    info{k}{1}(2) = y(k)
-    info{k}{2} = z(k)
+    info{k}{1}(1) = x(k);
+    info{k}{1}(2) = y(k);
+    info{k}{2} = z(k);
   end
 end
