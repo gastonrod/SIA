cffee9c78a0969ee45b37bdd4aa4d1c283b12397
Gaston <grodriguez@itba.edu.ar>
2018-03-22 14:34:12 -0300

    Improved the test.m and xor.m
    Gave more adecuate values to the xor learning script.
    Improved the test's documentation.

--- test.m
+++ test.m
@@ -1,14 +1,28 @@
-% patterns{k} : Input - Expected output pair
-function test(patterns)
-  eps = 1e-05;
+% This function takes a series of patterns and checks how well the network does.
+%
+% Paramters:
+%
+% W is a cell array of matrices. W{m}(j, i) holds the weight of the
+%   connection from unit i in the m-1 layer to unit j in the m layer
+%
+% patterns is a two dimentional cell array. patterns{i}{1} contains and input 
+%   pattern; patterns{i}{2} holds the expected output
+%
+% g is a two dimentional cell array of function handles. g{m}{1} is the the 
+%   activation function for layer m; g{m}{2} is the derivative of g{m}{1} in 
+%   terms of g (for example, if g{m}{1}(x) = tanh(x), then g{m}{2}(x) = 1-x^2
+%
+% eps is a scalar value that indicates how big the difference between expected 
+%   output and actual output can be. It's default value is 0.01.
+
+function test(W, patterns, g, eps= 0.1)
   err_count = 0;
   for k = [1:numel(patterns)]
-    % run : Give the input to the neural network and get an answer.
-    ans = run(patterns{k}{1});
-    if abs(ans- patterns{k}{2}) > eps
+    run_pattern(W, patterns{k}{1}, g);
+    output = ans{numel(ans)};
+    if abs(output - patterns{k}{2}) > eps
       err_count++;
     endif
   end
-  %print error percentage
   printf("%f%% of wrong answers\n", err_count / numel(patterns) * 100);
 end
--- xor.m
+++ xor.m
@@ -1,3 +1,4 @@
+% Learning patterns to be used in the learning and testing process
 patterns = {
  [{[0;0;0;0;0]} 0]
  [{[1;0;0;0;0]} 1]
@@ -8,17 +9,24 @@ patterns = {
  [{[1;1;1;0;0]} 1]
 };
 
+% Activation functions and their derivatives
 g = {
  [{@tanh} {@dtanh}]
  [{@tanh} {@dtanh}]
 };
 
+% Initial matrix of weights. First layer initialized with random small numbers.
 init_weights = {
  [rand(5,6)/10]
  [[0 0 0 0 0 0]]
 };
 
-eta = 0.5;
-epoch = 1000000;
+% Learning rate. 0.3 seems to be doing fine
+eta = 0.3;
+
+% Amount of times to loop through the patterns
+epoch = 1000;
 
 W = incremental_learn(init_weights, patterns, g, eta, epoch);
+
+test(W, patterns, g);
