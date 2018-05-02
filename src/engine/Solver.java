f8d4d5f1b2f560e8b28ed97af2f96f0f6b680b2d
foviedoITBA <foviedo@itba.edu.ar>
2018-05-01 22:56:15 -0300

    Remove non-agreed-upon non-interoperable code from the engine

--- src/engine/Solver.java
+++ src/engine/Solver.java
@@ -1,9 +1,6 @@
 package engine;
 
 import ar.com.itba.sia.*;
-import sokoban.SokobanDistanceHeuristic;
-import sokoban.SokobanPlacedBoxesHeuristic;
-import sokoban.SokobanProblem;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -14,26 +11,16 @@ import java.util.Properties;
 public class Solver {
 
     private static Search<?> search;
-    private static MyProperties properties;
-    public static boolean debug;
+    static boolean debug;
 
     public static void main(String[] args) {
-        Problem<?> problem = null;
         try {
+            MyProperties properties;
             properties = new MyProperties();
-            problem = new SokobanProblem(properties.getBoardFile());
-            System.out.println("Board parsing successful");
-        } catch (IOException e){
-            System.out.println("Properties loading went wrong");
-            System.out.println(e.getMessage());
-        }catch(Exception e) {
-            System.out.println("Board parsing went wrong");
-            System.out.println(e.toString());
-        }
-        try {
             debug = properties.getDebug();
-            solve((Problem<sokoban.SokobanState>) problem, properties.getHeuristic(), properties.getMethod());
-        } catch(Exception e){
+            solve(new sokoban.SokobanProblem("input4.txt"), new sokoban.SokobanDistanceHeuristic(), properties.getMethod());
+        } catch(Exception e) {
+            System.out.println("Unknown exception arose trying to solve game");
             System.out.println(e.getMessage());
         }
     }
@@ -77,35 +64,35 @@ public class Solver {
     }
 
     private static <E> Node<E> bfsSolve(Problem<E> problem, Heuristic<E> heuristic) {
-        Search<E> search =  new Search<E>();
+        Search<E> search =  new Search<>();
         Node<E> result = search.solve(problem, heuristic, false, Method.BFS);
         Solver.search = search;
         return result;
     }
 
     private static <E> Node<E> dfsSolve(Problem<E> problem, Heuristic<E> heuristic) {
-        Search<E> search =  new Search<E>();
+        Search<E> search =  new Search<>();
         Node<E> result = search.solve(problem, heuristic, false, Method.DFS);
         Solver.search = search;
         return result;
     }
 
     private static <E> Node<E> idaSolve(Problem<E> problem, Heuristic<E> heuristic) {
-        Search<E> search =  new Search<E>();
+        Search<E> search =  new Search<>();
         Node<E> result = search.solve(problem, heuristic, true, Method.DFS);
         Solver.search = search;
         return result;
     }
 
     private static <E> Node<E> greedySolve(Problem<E> problem, Heuristic<E> heuristic) {
-        Search<E> search =  new Search<E>();
+        Search<E> search =  new Search<>();
         Node<E> result = search.solve(problem, heuristic, false, Method.GREEDY);
         Solver.search = search;
         return result;
     }
 
     private static <E> Node<E> aStarSolve(Problem<E> problem, Heuristic<E> heuristic) {
-        Search<E> search =  new Search<E>();
+        Search<E> search =  new Search<>();
         Node<E> result = search.solve(problem, heuristic, false, Method.A_STAR);
         Solver.search = search;
         return result;
@@ -117,61 +104,42 @@ public class Solver {
         Node<E> solve(Problem<E> problem, Heuristic<E> heuristic);
     }
 
-    private static class MyProperties{
+    private static class MyProperties {
 
-        Properties properties;
+        private Properties properties;
 
-        public MyProperties() throws IOException{
+        MyProperties() throws IOException {
             properties = new Properties();
             properties.load(new FileInputStream("config.properties"));
         }
 
-        public Heuristic getHeuristic() throws Exception{
-            String s = properties.getProperty("HEURISTIC");
-            if (s == null)
-                throw new Exception("Heuristic not found in the config.properties file.");
-            if (s.equals("DISTANCE")){
-                return new SokobanDistanceHeuristic();
-            }else if(s.equals("PLACED_BOXES")){
-                return new SokobanPlacedBoxesHeuristic();
-            }else{
-                throw new Exception("The heuristic written in the properties file is wrong.");
-            }
-        }
-
-        public SolveMethod getMethod() throws Exception{
+        SolveMethod getMethod() throws Exception {
             String s = properties.getProperty("METHOD");
-            if (s == null)
+            if (s == null) {
                 throw new Exception("Method not found in the config.properties file.");
-            if ( s.equals("A_STAR")){
-                return Solver::aStarSolve;
-            }else if (s.equals("BFS")){
-                return Solver::bfsSolve;
-            }else if (s.equals("DFS")){
-                return Solver::dfsSolve;
-            }else if (s.equals("GREEDY")){
-                return Solver::greedySolve;
-            }else if ( s.equals("IDA")){
-                return Solver::idaSolve;
-            } else {
-                throw new Exception("The method written in the properties file is wrong.");
+            }
+            switch (s) {
+                case "A_STAR":
+                    return Solver::aStarSolve;
+                case "BFS":
+                    return Solver::bfsSolve;
+                case "DFS":
+                    return Solver::dfsSolve;
+                case "GREEDY":
+                    return Solver::greedySolve;
+                case "IDA":
+                    return Solver::idaSolve;
+                default:
+                    throw new Exception("The method written in the properties file is invalid.");
             }
         }
 
-        public boolean getDebug() throws Exception{
+        boolean getDebug() throws Exception {
             String s = properties.getProperty("DEBUG");
-            if (s == null)
+            if (s == null) {
                 throw new Exception("Debug not found in the config.properties file.");
+            }
             return Boolean.parseBoolean(s);
         }
-
-        public String getBoardFile() throws Exception{
-            String s = properties.getProperty("BOARD");
-            if (s == null)
-                throw new Exception("Board filename not found in the config.properties file.");
-            return s;
-
-        }
     }
-
 }
