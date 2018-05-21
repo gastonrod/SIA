a1b1776b81d7ba966f3c4fad938b1baf0fa65117
Gaston <grodriguez@itba.edu.ar>
2018-05-21 15:32:57 -0300

    Added AnnularCrosser. Now using Random instead of Math.rand()

--- src/engine/crossover/AnnularCrosser.java
+++ src/engine/crossover/AnnularCrosser.java
@@ -5,21 +5,23 @@ import engine.model.Pair;
 
 import java.util.Random;
 
-public class SinglePointCrosser implements Crosser {
+public class AnnularCrosser implements Crosser{
+
 
     @Override
     public Pair<Individual> cross(Pair<Individual> pair) {
         Random rand = new Random();
-        int r = rand.nextInt(pair.first.getLocusAmount());
+        int locusAmount = pair.first.getLocusAmount();
+        int r = rand.nextInt(locusAmount);
+        int l = rand.nextInt((int) (locusAmount  / 2.0 - 1)) + 1;
         Pair<Individual> children = new Pair<>();
 
         children.first = pair.first.replicate() ;
         children.second = pair.second.replicate();
 
-        for(int i = r; i < pair.first.getLocusAmount(); i++){
-            children.first.exchangeAt(children.second, i);
+        for(int i = 0; i < l; i++){
+            children.first.exchangeAt(children.second, (r + i) % locusAmount);
         }
-
         return children;
     }
 }
--- src/engine/crossover/TwoPointCrosser.java
+++ src/engine/crossover/TwoPointCrosser.java
@@ -4,22 +4,26 @@ import engine.engineException.InvalidLocusAmountException;
 import engine.model.Individual;
 import engine.model.Pair;
 
+import java.util.Random;
+
 public class TwoPointCrosser implements Crosser{
     @Override
     public Pair<Individual> cross(Pair<Individual> pair) {
         if(pair.first.getLocusAmount() < 2) {
             throw new InvalidLocusAmountException();
         }
-
-        int locus1 = (int) (Math.random() * pair.first.getLocusAmount());
-        int locus2;
-        while ((locus2 = (int) (Math.random() * pair.first.getLocusAmount())) == locus1);
+        Random rand = new Random();
+        int r1 = rand.nextInt(pair.first.getLocusAmount());
+        int r2;
+        while ((r2 = rand.nextInt(pair.first.getLocusAmount())) == r1);
         Pair<Individual> children = new Pair<>();
 
         children.first = pair.first.replicate() ;
         children.second = pair.second.replicate();
-        children.first.exchangeAt(children.second,Math.min(locus1,locus2));
-        children.first.exchangeAt(children.second,Math.max(locus1,locus2));
+
+        for(int i = Math.min(r1,r2); i < Math.max(r1,r2); i++){
+            children.first.exchangeAt(children.second, i);
+        }
 
         return children;
     }
