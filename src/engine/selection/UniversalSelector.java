ea05b53fc54ea381e44e94da022d312ed03ca5fc
Gaston <grodriguez@itba.edu.ar>
2018-05-21 23:05:46 -0300

    Added an abstract selector to avoid repeating code. Added the UniversalSelector.

--- src/engine/selection/UniversalSelector.java
+++ src/engine/selection/UniversalSelector.java
@@ -5,16 +5,19 @@ import engine.model.Individual;
 import java.util.List;
 
 
-public class RouletteSelector extends AccumulatedSumSelector implements Selector {
+public class UniversalSelector extends AccumulatedSumSelector implements Selector {
 
     @Override
     public List<Individual> select(List<Individual> population, int k, FitnessFunction fitnessFunction) {
         super.init(population, k, fitnessFunction);
+        double r = Math.random();
 
-        for(int i = 0; i < k; i++) {
-            selectedIndividuals.add(pop[super.search(accumulatedSum, Math.random())]);
+        for(int i = 1; i <= k; i++) {
+            double rand = (r + (double) i - 1.0) / (double) k;
+            selectedIndividuals.add(pop[super.search(accumulatedSum, rand)]);
         }
         return selectedIndividuals;
     }
 
 }
+
