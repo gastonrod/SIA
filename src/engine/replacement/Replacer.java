0d0e1bbc68b36f993daf17ff639fd5a96d9478ad
Axel <afratoni@itba.edu.ar>
2018-05-25 17:39:24 -0300

    Replacer refactor

--- src/engine/replacement/Replacer.java
+++ src/engine/replacement/Replacer.java
@@ -2,10 +2,11 @@ package engine.replacement;
 
 import engine.model.Individual;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @FunctionalInterface
 public interface Replacer {
 
-    List<Individual> replace(List<Individual> population, List<Individual> children, double generationalGap);
+    void replace(ArrayList<Individual> population, ArrayList<Individual> children, double generationalGap);
 }
--- src/engine/replacement/SimpleReplacer.java
+++ src/engine/replacement/SimpleReplacer.java
@@ -3,20 +3,19 @@ package engine.replacement;
 import engine.engineException.InvalidGenerationGapException;
 import engine.model.Individual;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class SimpleReplacer implements Replacer {
 
     @Override
-    public List<Individual> replace(List<Individual> population, List<Individual> children, double generationalGap) {
-        int oldGenerationRemnants = (int) (population.size() * (1 - generationalGap));
-        int newComers = population.size() - oldGenerationRemnants;
-        boolean canFillGap = newComers <= children.size();
-        if(!canFillGap) {
-            throw new InvalidGenerationGapException();
+    public void replace(ArrayList<Individual> population, ArrayList<Individual> children, double generationalGap) {
+        Collections.shuffle(population);
+        for(int i = 0; i < children.size(); i++) {
+            population.remove(population.size() - 1);
         }
-        List<Individual> nextGeneration = population.subList(0,oldGenerationRemnants);
-        nextGeneration.addAll(children.subList(0,newComers));
-        return nextGeneration;
+        population.addAll(children);
     }
 }
